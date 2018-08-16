c8ef06c36021ba4be27bf3956a0b2007727f3c85
pedrotorchio <pedrotorchio@gmail.com>
2018-08-15 21:13:13 -0300

    including wrapper in plugin

--- src/plugin.js
+++ src/plugin.js
@@ -1,7 +1,9 @@
 import ResponsiveImage from "./ResponsiveImage.vue";
+import ResponsiveImageWrapper from "./ResponsiveImageWrapper.vue";
 
 module.exports = {
   install: function(Vue, options) {
     Vue.component("ResponsiveImage", ResponsiveImage);
+    Vue.component("ResponsiveImageWrapper", ResponsiveImageWrapper);
   }
 };
